56f3f5866437aadb0a904f8795de286bb6ef92b6
Uni <jan.bundesmann@physik.uni-regensburg.de>
2012-10-16 11:25:51 +0200

    README:     new file with usage instructions
    evtools/plotg.py:
        added "--help" command line option
        --logarithmic now creates a logarithmic normalization of the colors

--- evtools/plotg.py
+++ evtools/plotg.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.ticker as mticker
+import matplotlib.colors as mcolors
 import os
 import sys
 import getopt
@@ -35,6 +36,26 @@ def plotg(dataFileName, **opts):
 
     plot_mode = "image"
 
+    # processing of options that are needed to fetch the data
+    for opt,val in opts.items():
+        if opt == "-c":
+            z_col = val
+        if opt == "-N":
+            N = int(val)
+
+    # fetch the data
+    data = MultiMap(dataFileName)
+
+    x,y,z,extent = data.retrieve_3d_plot_data("1", "2", z_col, N = N)
+
+    print "xrange: ", np.min(x), np.max(x)
+    print "yrange: ", np.min(y), np.max(y)
+    plot_options["vmin"] = np.min(z)
+    plot_options["vmax"] = np.max(z)
+    print "zrange: ", plot_options["vmin"], plot_options["vmax"]
+
+
+    # process options for the creation of the plot
     for opt,val in opts.items():
         if opt == "--pdf": save_pdf = True
         if opt == "--eps": save_eps = True
@@ -47,8 +68,6 @@ def plotg(dataFileName, **opts):
             ylim = val
         if opt == "--title":
             plt.title(val)
-        if opt == "-c":
-            z_col = val
         if opt == "-m":
             if val == "spin":
                 plot_options["cmap"] = my_cm.spin
@@ -74,28 +93,23 @@ def plotg(dataFileName, **opts):
             levels = [float(x) for x in levels]
             contour_opts["levels"] = levels
 
-        if opt == "-N":
-            N = int(val)
-
         if opt == "--interpolation":
             plot_options["interpolation"] = val
 
         if opt == "-g" or opt == "--grid":
             show_grid = True
 
+    # a second run for options that might need the procession of another option
+    # already before they are processed
+    for opt,val in opts.items():
+        if opt == "-l" or opt == "--logarithmic":
+            plot_options["norm"] = mcolors.LogNorm(
+                    plot_options["vmin"], plot_options["vmax"])
 
-    data = MultiMap(dataFileName)
-
-    x,y,z,extent = data.retrieve_3d_plot_data("1", "2", z_col, N = N)
-
-    print "xrange: ", np.min(x), np.max(x)
-    print "yrange: ", np.min(y), np.max(y)
-    print "zrange: ", np.min(z), np.max(z)
 
     if show_grid is True:
         plt.plot(x, y, "k,")
 
-    #z = np.ma.masked_array(z, mask=np.isnan(z))
     result = ""
     if plot_mode is "image":
         result = plt.imshow(z, extent = extent, **plot_options)
@@ -110,6 +124,18 @@ def plotg(dataFileName, **opts):
     if modify_ylim:
         ylimits = ylim.split( ":" )
         plt.ylim( float( ylimits[0] ), float( ylimits[1] ) )
+    
+    if show_colorbar is True:
+        cax = plt.axes([0.7,0.1,0.18,0.08])
+        plt.colorbar(cax=cax, orientation = "horizontal")
+        labels = cax.get_xticklabels()
+        for label in labels:
+                label.set_rotation(30) 
+                label.set_ha("right")
+                label.set_size("small")
+
+    ax.xaxis.set_major_locator(mticker.NullLocator())
+    ax.yaxis.set_major_locator(mticker.NullLocator())
 
     # save to eps
     if save_eps:
@@ -127,22 +153,50 @@ def plotg(dataFileName, **opts):
         outfileName = dataFileName.replace(".dat",".png")
         plt.savefig( outfileName )
 
-    if show_colorbar is True:
-        cax = plt.axes([0.7,0.1,0.18,0.08])
-        plt.colorbar(cax=cax, orientation = "horizontal")
-        labels = cax.get_xticklabels()
-        for label in labels:
-                label.set_rotation(30) 
-                label.set_ha("right")
-                label.set_size("small")
-
     return result
+
+def usage():
+    print textwrap.dedent("""\
+    Usage: plotg.py [OPTIONS] ... datafilename
+    Creates a color-coded image plot according to x,y and z data found in 
+    'datafilename' in particular columns
+
+    Mandatory arguments to long options are mandatory for short options too.
+    -c, --col=NUMBER            use the columns NUMBER for z
+    -N=BINS                     average over BINS data points, effectively reducing
+                                the number of datapoint for the plot
+        --interpolation=NAME    the name of an allowed interpolation method of
+                                the pyplot.imshow method, currently allowed values
+                                are: 'nearest', 'bilinear', 'bicubic', 'spline16', 
+                                'spline36', 'hanning', 'hamming', 'hermite', 
+                                'kaiser', 'quadric', 'catrom', 'gaussian', 'bessel', 
+                                'mitchell', 'sinc', 'lanczos'
+                                Defaults to nearest
+    -b                          plot a colorbar-legend
+    -g, --grid                  plot the grid behind the color-image
+    -m=NAME                     the NAME of the desired colormap, possible values are
+                                spin, hot, gray and jet
+    -z LOWER:UPPER              limits the z-axis to a given range
+        --eps                   saves the plot as an eps file
+        --pdf                   saves the plot as a pdf file
+        --png                   saves the plot as a png file
+        --xlim=LOWER:UPPER
+        --ylim=LOWER:UPPER
+        --title=STRING
+    -l, --logarithmic           make a logarithmic color-coding
+        --interpolation
+    -h, --help                  shows this explanation
+    """)
 
 if __name__ == "__main__":
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'c:m:z:bN:g', 
+        opts, args = getopt.getopt(sys.argv[1:], 'c:m:z:bN:glh', 
                                    ['eps','pdf','png','xlim=','ylim=','title=',
-                                       'interpolation=', 'grid'])
+                                       'interpolation=', 'grid', "logarithmic",
+                                       "help"])
+
+        if ("--help", '') in opts:
+            raise getopt.GetoptError("")
 
         if len(args) > 0 : dataFileName = args[0]
         else : dataFileName = "scalars.out"
@@ -153,38 +207,7 @@ if __name__ == "__main__":
         opts = dict(opts)
         plotg(dataFileName, **opts)
 
-        ax.xaxis.set_major_locator(mticker.NullLocator())
-        ax.yaxis.set_major_locator(mticker.NullLocator())
-
         plt.show()
     except getopt.GetoptError:
-        print textwrap.dedent("""\
-        Usage: plotg.py [OPTIONS] ... datafilename
-        Creates a color-coded image plot according to x,y and z data found in 
-        'datafilename' in particular columns
-
-        Mandatory arguments to long options are mandatory for short options too.
-        -c, --col=NUMBER            use the columns NUMBER for z
-        -N=BINS                     average over BINS data points, effectively reducing
-                                    the number of datapoint for the plot
-            --interpolation=NAME    the name of an allowed interpolation method of
-                                    the pyplot.imshow method, currently allowed values
-                                    are: 'nearest', 'bilinear', 'bicubic', 'spline16', 
-                                    'spline36', 'hanning', 'hamming', 'hermite', 
-                                    'kaiser', 'quadric', 'catrom', 'gaussian', 'bessel', 
-                                    'mitchell', 'sinc', 'lanczos'
-                                    Defaults to nearest
-        -b                          plot a colorbar-legend
-        -g, --grid                  plot the grid behind the color-image
-        -m=NAME                     the NAME of the desired colormap, possible values are
-                                    spin, hot, gray and jet
-        -z LOWER:UPPER              limits the z-axis to a given range
-            --eps                   saves the plot as an eps file
-            --pdf                   saves the plot as a pdf file
-            --png                   saves the plot as a png file
-            --xlim=LOWER:UPPER
-            --ylim=LOWER:UPPER
-            --title=STRING
-            --interpolation
-        """)
+        usage()
 
