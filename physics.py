9fcc393c68ca0e8a282b576cfec4352cc3538f22
Jan Bundesmann (trurl) <jan@veskamyr.de>
2015-02-03 13:37:30 +0100

    Started the creation of a documentation
    Since the project in the near future shall be published I started
    creating a sphinx documentation.
    In order to fully understand the git-flow things I choose extra-small
    commits.

--- physics.py
+++ physics.py
@@ -2,11 +2,12 @@
 
 
 def GammaS_to_SpinRelaxationRate(GammaS, W, rho):
-    """ transforms spin flip probability GammaS to a spin relaxation rate
-        We assume a nanoribbon of width W containing one adatom.
-        rho is the adatom density we're calculating the rate for.
-        W has to be given in units of a
-        """
+    """
+    transforms spin flip probability GammaS to a spin relaxation rate
+    We assume a nanoribbon of width W containing one adatom.
+    rho is the adatom density we're calculating the rate for.
+    W has to be given in units of a
+    """
     t = 2.6
     a = 1
     hbar = 6.58e-16
